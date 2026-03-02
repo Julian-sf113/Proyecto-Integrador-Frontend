82be1f76e5feae324cdd6c79529a683bea2ef87a
DavidPineda02 <davxpa.02@gmail.com>
2026-03-02 18:04:19 -0500

    Implementar sistema de notificaciones toast reutilizable e independiente
    - Crear utils/notificaciones.js con tres tipos: exito, error e info
    - Las notificaciones se cierran automaticamente y tienen boton de cierre manual
    - Registrar las exportaciones en utils/index.js
    - Agregar estilos de animacion de entrada y salida en notifications.css

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,3 @@
 export { validar, esTextoValido } from './validaciones.js';
+// RF03: Notificaciones toast independientes
+export { notificarExito, notificarError, notificarInfo } from './notificaciones.js';
